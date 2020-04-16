5ae3c557d86f7a3f23a78a8e132aaf531cfc987c
vk-it <46228289+vk-it@users.noreply.github.com>
2020-04-16 22:06:11 +0300

    Дополнение задания 2

--- js/script.js
+++ js/script.js
@@ -73,12 +73,12 @@ let mainList = {
 for (let i = 0; i < 5; i++) {
 
     let a = prompt('Какой тип товаров будем продавать?');
-
-    if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50 ) {
+ 
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
         console.log('Всё верно!');
         mainList.shopGoods[i] = a;
     } else {
-        // i = i - 1;
+        i = i - 1;
     }
 }
 
