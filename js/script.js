eaa4c0847e317ea683472bc51c7bb551e50f227c
vk-it <46228289+vk-it@users.noreply.github.com>
2020-04-17 23:01:49 +0300

    Переписал for в while и do while

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ let mainList = {
 for (let i = 0; i < 5; i++) {
 
     let a = prompt('Какой тип товаров будем продавать?');
- 
+
     if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
         console.log('Всё верно!');
         mainList.shopGoods[i] = a;
@@ -97,3 +97,33 @@ console.log(mainList)
 // } else {
 //     console.log('В сутках только 24 часа');
 // }
+
+// Переписать цикл for в whike и do while
+
+// let i = 0;
+
+// while (i < 5) {
+//     let a = prompt('Какой тип товаров будем продавать?');
+
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
+//         console.log('Всё верно!');
+//         i++
+//         mainList.shopGoods[i] = a;
+
+//     } else {
+//         i = i - 1;
+//     }
+// }
+
+// do {
+//     let a = prompt('Какой тип товаров будем продавать?');
+
+//     if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
+//         console.log('Всё верно!');
+//         i++
+//         mainList.shopGoods[i] = a;
+
+//     } else {
+//         i = i - 1;
+//     }
+// } while (i < 5);
